b650d51782d9953e86550c44504692289dcff235
Matt Drake <drake.mattc@gmail.com>
2021-09-10 00:09:06 -0400

    Refactor date stringbuilding into a function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -7,16 +7,25 @@ interface TaskListProps {
   tasks: Task[];
 }
 
+// Builds due date strings, used by the task header list.
+const buildDateString = (date: Date) => {
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month}/${day}/${year}`;
+};
+
 const TaskList = (props: TaskListProps) => {
-  const taskHeaders = props.tasks.map((task) => (
-    <li className="flow-root px-4">
+  const taskHeaders = props.tasks.map((task, index) => (
+    <li className="border flow-root px-4" key={index}>
       <span className="float-left">{task.title}</span>
       <span className="float-right">
-        <span className="mx-4">
-          {task.dueDate.getMonth() + 1}/{task.dueDate.getDate()}/
-          {task.dueDate.getFullYear()}
+        <span className="mx-4">{buildDateString(task.dueDate)}</span>
+        <span>
+          {task.difficulty}
+          {String.fromCodePoint(0x2605)}
         </span>
-        <span>{task.difficulty}</span>
       </span>
     </li>
   ));
